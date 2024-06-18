8200f4270b3c6171b22ae09bb080a777e2e9c3a5
isma-sylla <ismaila.sylla@edu.esiee.fr>
2024-06-18 19:51:05 +0200

    backend commit

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,63 +1,141 @@
+import sqlite3
 from flask import Flask, request
+from services import AuthServices
+from services import RequestServices
 from flask_cors import CORS
-import hashlib
-from jwt_utils import build_token, decode_token
-from models import Question
-from crud import create_question
-from serializers import dict_to_question
-import database
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/')
-def hello_world():
-	x = 'world'
-	return f"Hello, {x}"
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
-	return {"size": 0, "scores": []}, 200
+
+    nb_question, scores = RequestServices.get_user_infos()
+    return {'size': nb_question, 'scores': scores}, 200
 
 
-@app.route('/login', methods=['POST'])
-def login():
-	payload = request.get_json()
-	tried_password = payload['password'].encode('UTF-8')
-	hashed = hashlib.md5(tried_password).digest()
-	token = build_token()
-	if hashed == b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@':
-		return {'token' : token}, 200
-	else:
-		return {"error": "Unauthorized"}, 401
-
-@app.route('/questions', methods=['POST'])
-def post_question():
-    token = request.headers.get('Authorization')
-    if not token:
-        return {"error": "Authorization token is missing"}, 400
-
-    payload = request.get_json()
-    question = dict_to_question(payload)
-    
-    create_question(question)
-    
-    return {"message": "Question added successfully"}, 200
-
-@app.route('/rebuild-db', methods=['POST'])
-def rebuild_db():
-    token = request.headers.get('Authorization')
-    if not token:
-        return {"error": "Authorization token is missing"}, 400
+@app.route('/participations', methods=['POST'])
+def PostAnswers():
 
     try:
-        # Décodez et vérifiez le token
-        decode_token(token)
+        good_answers, position_answers, score, player_name = RequestServices.post_answers(
+            request.get_json())
+    except IndexError as e:
+        return {'error': str(e)}, 400
+    except sqlite3.IntegrityError as e:
+        return str(e), 409
     except Exception as e:
-        return {"error": "Unauthorized"}, 401
-    
-    # Réinitialisez la base de données
-    database.init_db()
-    return {"message": "Database rebuilt successfully"}, 200
+        return str(e), 500
 
-if __name__ == "__main__":
+    return {
+        'answersSummaries ': {
+            'correctAnswerPosition': position_answers,
+            'wasCorrect': good_answers
+        },
+        'playerName': player_name,
+        'score': score
+    }, 200
+
+
+@app.route('/participations', methods=['DELETE'])
+def DeleteParticipants():
+
+    if not AuthServices.verify_token(request.headers):
+        return '', 401
+
+    try:
+        RequestServices.delete_participants()
+    except Exception as e:
+        return str(e), 500
+
+    return {}, 204
+
+
+@app.route('/questions', methods=['GET'])
+def GetQuestions():
+
+    question_list = RequestServices.get_questions()
+    return {"questions": question_list, "size": len(question_list)}, 200
+
+
+@ app.route('/questions/<position>', methods=['GET'])
+def GetQuestion(position):
+    if not RequestServices.verifyPosition(position):
+        return 'position not found', 404
+
+    try:
+        question = RequestServices.get_question(position)
+    except Exception as e:
+        return str(e), 500
+
+    return question, 200
+
+
+@ app.route('/questions/<position>', methods=['DELETE'])
+def DeleteQuestion(position):
+
+    if not AuthServices.verify_token(request.headers):
+        return '', 401
+
+    if not RequestServices.verifyPosition(position):
+        return 'Position not found', 404
+
+    try:
+        RequestServices.delete_question(position)
+    except Exception as e:
+        return str(e), 500
+
+    return '', 204
+
+
+@ app.route('/questions/<position>', methods=['PUT'])
+def PutQuestion(position):
+    if not AuthServices.verify_token(request.headers):
+        return '', 401
+
+    if not RequestServices.verifyPosition(position):
+        return 'position not found', 404
+
+    payload = request.get_json()
+
+    try:
+        RequestServices.put_question(position, payload)
+    except sqlite3.IntegrityError as e:
+        return str(e), 409
+    except Exception as e:
+        return str(e), 500
+
+    return '', 204
+
+
+@ app.route('/questions', methods=['POST'])
+def PostQuestion():
+
+    payload = request.get_json()
+
+    if not AuthServices.verify_token(request.headers):
+        return '', 401
+
+    try:
+        RequestServices.post_question(payload)
+    except sqlite3.IntegrityError as e:
+        return str(e), 409
+    except Exception as e:
+        return str(e), 500
+
+    return '', 200
+
+
+@ app.route('/login', methods=['POST'])
+def Login():
+    try:
+        token_string = AuthServices.login(request.get_json()['password'])
+        return {"token": token_string}, 200
+    except ValueError as e:
+        return str(e), 401
+
+
+if __name__ == '__main__':
+
+    # use_reloader=True, debug=True
     app.run()
--- quiz-api/models.py
+++
@@ -1,6 +0,0 @@
-class Question:
-    def __init__(self, position: int, title: str, text: str, image: str = None):
-        self.position = position
-        self.title = title
-        self.text = text
-        self.image = image
